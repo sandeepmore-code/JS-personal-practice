366917a0b32d11d5bc5e3e801067d03202e448f7
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-06 22:26:15 +0530

    updates

---
+++ 05-02-2024/practice.js
@@ -0,0 +1,59 @@
+//datatypes- Strings,Numbers,Boolean.
+
+//string-
+
+//name=""
+
+// console.log("sandeep")
+
+// //number=without invertetd comma
+
+// console.log("1234")
+
+// //Boolean- True Or Flase
+
+// console.log(true, typeof true)
+
+// console.log("sandeep")
+// console.log(typeof "sandeep@1234")
+
+// console.log('Sandeep')
+// console.log (typeof ('true'))
+
+// console.log (`sandeep`)
+// console.log ( typeof`12.23`)
+
+// console.log("2" + '2')
+// console.log(2+2)
+
+// console.log('1234' , typeof '1234')
+// console.log(1234,typeof 1234)
+// console.log(true , typeof true)
+
+//variable
+
+// var mobile= 114
+// var mobile2=112
+
+// console.log('mobile'+'mobile2')
+
+// var awdiz=50
+// console.log(awdiz)
+// var awdiz=60
+// console.log(awdiz)
+// var awdiz=80
+// console.log(awdiz)
+
+// let ppl=50
+// console.log(ppl)
+//  ppl=60
+// console.log(ppl) 
+// let ppl=70
+// console.log(ppl)
+
+const CPL=50
+console.log(CPL)
+ CPL=60
+ console.log(CPL)
+ const CPL=70// cannot re-assign or re-declare.
+ 
