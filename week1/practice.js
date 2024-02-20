7a10f3b624fa93a007ebefa79a03ac912fba6c01
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-02-20 23:38:46 +0530

    updates

---
+++ week1/practice.js
@@ -0,0 +1,27 @@
+
+// Q-Find the addition of target value form array
+
+
+// var array=[1,2,4,56,7,8,3,2,5,7]
+// target=12
+
+// function Findtarget( array,target){
+//     for(var i=0;i<array.length-1;i++){
+//         for (var j=i+1;j<= array.length; j++){
+//             if(array[i]+ array [j]== target){
+//                 console.log(array[i],array[j])
+//             }
+//         }
+//     }
+// }
+
+// Findtarget(array,target)
+
+// Q-Nested loop.
+
+for(i=0;i>=3;i++){
+    for(j=0;j<=4;j++){
+
+    }
+    console.log(i,j)
+}
